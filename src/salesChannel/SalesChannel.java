6e25253ad4b366e62ba8ae1aaa9f24bd14070f02
KrzNaw <krzysztof.nawrocki@poczta.onet.pl>
2017-12-10 11:24:45 +0100

    working on buying algorithm

--- src/salesChannel/SalesChannel.java
+++ src/salesChannel/SalesChannel.java
@@ -7,19 +7,25 @@ public class SalesChannel {
     private Product product;
 
     public void add(Product product) {
+        System.out.println("Add product: " + product);
         this.product = product;
     }
 
     public Product get() {
+        System.out.println("Get product: " + product);
         return product;
+    }
+
+    public boolean isBusy(){
+        return products != null;
     }
 
     public static void main(String[] args) {
         SalesChannel salesChannel = new SalesChannel();
-        Producer producer = new Producer(salesChannel);
+        List<Product> products = new ArrayList<>();
+        Producer producer = new Producer(salesChannel, products);
         Consumer consumer = new Consumer(salesChannel);
 
-        List<Product> products = new ArrayList<>();
         products.add(new Product("Vodka"));
         products.add(new Product("Beer"));
         products.add(new Product("Herring"));
@@ -27,25 +33,50 @@ public class SalesChannel {
         products.add(new Product("Scotch"));
         products.add(new Product("Cognac"));
 
+        Thread producerThread = new Thread(producer);
+        producerThread.start();
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
+
+        producer.getAmountOfSold();
+        consumer.getAmountOfBought();
+
+
+
     }
 
 }
 
 
-class Producer {
+class Producer implements Runnable {
     private SalesChannel salesChannel;
+    private List<Product> products;
 
-    public Producer(SalesChannel salesChannel) {
+    public Producer(){}
+
+    public Producer(SalesChannel salesChannel, List<Product> products) {
         this.salesChannel = salesChannel;
+        this.products = products;
     }
 
     void putProductIntoChannel(Product product) {
-        salesChannel.add(product);
+                salesChannel.add(product);
+    }
+
+    @Override
+    public void run() {
+        for(Product product : products){
+            System.out.println("Selling product: " + product);
+            this.putProductIntoChannel(product);}
+    }
+
+    int getAmountOfSold(){
+
     }
 }
 
 
-class Consumer {
+class Consumer implements Runnable {
     private SalesChannel salesChannel;
 
     public Consumer(SalesChannel salesChannel) {
@@ -53,20 +84,40 @@ class Consumer {
     }
 
     void buyProductFromChannel() {
-        Product product = salesChannel.get();
+        if(salesChannel.isBusy()) {
+            Product product = salesChannel.get();
+            System.out.println("Product bought: " + product);
+        }
     }
 
+    @Override
+    public void run() {
+        buyProductFromChannel();
+    }
+
+    public int getAmountOfBought(){
+
+    }
 }
 
 
 class Product {
-    private String nazwa;
+    private String name;
 
-    public Product(String nazwa) {
-        this.nazwa = nazwa;
+    public Product(String name) {
+        this.name = name;
     }
 
-    public String getNazwa() {
-        return nazwa;
+    public String getName() {
+        return name;
     }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+        
 }
